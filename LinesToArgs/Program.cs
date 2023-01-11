e7f8cc8b10f79f46142b0f25de8bb1ebfdce48b6
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-01-12 06:56:48 +0800

    added replacer. doesn't work with piping

--- LinesToArgs/Program.cs
+++ LinesToArgs/Program.cs
@@ -6,7 +6,12 @@ internal class Program
 {
    private static void Main(string[] args)
    {
-      string output = Regex.Replace(args[0], "1", "2");
+      string input = args[0];
+      string pattern = "";
+      string replacement = "";
+
+      string output = Regex.Replace(input, pattern, replacement);
+
       Console.WriteLine(output);
    }
 }
