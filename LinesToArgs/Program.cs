508089f7d2290a2fb0264f6f3e8ec1bf667ff60f
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-01-12 06:40:42 +0800

    trying out how regex works in C#

---
+++ LinesToArgs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LinesToArgs;
+
+internal class Program
+{
+   private static void Main(string[] args)
+   {
+      string output = Regex.Replace(args[0], "1", "2");
+      Console.WriteLine(output);
+   }
+}
