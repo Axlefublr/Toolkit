8384a317e76bb99c663c46d67dd2b81ca2128144
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-02-14 20:58:28 +0800

    Made the path converter

---
+++ PathConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PathConverter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        foreach (string path in args) {
+            Console.WriteLine(ConvertPath(path));
+        }
+    }
+
+    private static string ConvertPath(string path) {
+        path = path.Replace(@"/mnt/c/", @"C:\\").Replace(@"/", @"\\");
+        return path;
+    }
+}
